ec76b0ce0a559cdd9185a4c18ae5a369be1067b5
Nikolay Evsukov <Nikolasgrizli@users.noreply.github.com>
2021-12-10 05:40:12 +0200

    with svg icons plugin

--- src/js/components/hello.js
+++ src/js/components/hello.js
@@ -1,2 +1,11 @@
-console.log('hello from component');
+// import box from '../../icons/box.svg';
 
+// const elem = document.getElementById('test');
+// if(!!elem){
+//     console.log('test')
+//     const icon = `
+//         <svg>
+//         <use xlink:href="${box}" />
+//         </svg>`;
+//     elem.innerHTML = icon;
+// }
